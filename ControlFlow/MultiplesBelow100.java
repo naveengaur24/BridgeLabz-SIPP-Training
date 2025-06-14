86529bffcd52b64555515e070e0e514aa339c1bd
Naveen gaur <naveengaur343@gmail.com>
2025-06-15 00:08:21 +0530

    [Naveen Gaur] Add . Multiple below 100

---
+++ ControlFlow/MultiplesBelow100.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class MultiplesBelow100 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (num > 0 && num < 100) {
+            for (int i = 100; i >= 1; i--) {
+                if (i % num == 0)
+                 System.out.println(i);
+            }
+        }
+        else{
+            System.out.println("Invalid input");
+        }
+    }
+}

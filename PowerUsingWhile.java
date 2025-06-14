086548dfd8d3e89b51f31181e5e3ac18237f2ec6
Naveen gaur <naveengaur343@gmail.com>
2025-06-14 20:12:17 +0530

    [Naveen Gaur] Add . Power of a number using while loop.

---
+++ PowerUsingWhile.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class PowerUsingWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter base number");
+        int n = sc.nextInt();
+        System.out.print("Enter power");
+        int pow= sc.nextInt();
+        int result = 1;
+        int counter = 0;
+        while (counter < pow) {
+            result *= n;
+            counter++;
+        }
+        System.out.println(n + "raised to the power" + pow + " is" +result);
+    }
+}

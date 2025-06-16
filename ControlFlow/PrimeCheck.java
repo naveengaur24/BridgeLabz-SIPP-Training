d63f57e620c207e8a3b0c996a9877e5627882844
Naveen gaur <122592035+naveengaur24@users.noreply.github.com>
2025-06-16 08:42:48 +0530

    Update PrimeCheck.java

--- ControlFlow/PrimeCheck.java
+++ ControlFlow/PrimeCheck.java
@@ -4,8 +4,8 @@ class PrimeCheck {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         boolean isPrime = number > 1;
-        for (int i = 2; i <= number / 2 && isPrime; i++) {
-            if (number % i == 0) {
+        for (int i = 2; i <= n / 2 && isPrime; i++) {
+            if (n % i == 0) {
                 isPrime = false;
             }
         }

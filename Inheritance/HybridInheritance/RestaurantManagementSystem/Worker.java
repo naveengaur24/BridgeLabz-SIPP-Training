1f0c11c0b17c518ce6a891f8617790ce965f1440
Naveen gaur <122592035+naveengaur24@users.noreply.github.com>
2025-08-21 10:22:56 +0530

    .

--- Inheritance/HybridInheritance/RestaurantManagementSystem/Worker.java
+++
@@ -1,7 +0,0 @@
-abstract class Worker extends Person {
-    Worker(String name, int id) {
-        super(name, id);
-    }
-
-    abstract void performDuties();
-}
--- Inheritance/HybridInheritance/VehicleManagementSystem/Refuelable.java
+++
@@ -1,3 +0,0 @@
-abstract class Refuelable {
-    abstract void refuel();
-}
